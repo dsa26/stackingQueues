8257127af69147b4d4aceddc1f4ec7820bbefa1f
Zohan Subhash <zohan.subhash@gmail.com>
2025-10-15 12:29:25 -0400

    Fixed StackRA

--- StackLL.java
+++ StackLL.java
@@ -7,6 +7,7 @@ public class StackLL<E> {
 
     public void push(E element) {
         this.TOP = new Node<E>(element, this.TOP);
+        E[] temp = new <E>[2];
     }
 
     public E pop() {
--- StackRA.java
+++ StackRA.java
@@ -2,47 +2,57 @@ public class StackRA<E> {
     private E[] stack;
     private int blockSize;
     private int currentSize;
-    private int buffer;
+    private int bufferSize;
 
     public StackRA(int buffer) {
-        if (buffer < 1) throw new 
+        if (buffer < 1)
+            throw new IllegalArgumentException("Buffer size has to be 1 or greater");
+        this.bufferSize = buffer;
+        this.blockSize = buffer;
+        this.currentSize = 0;
+        this.stack = (E[]) new Object[blockSize];
     }
 
     public StackRA() {
-        StackRA(10);
+        this(10);
     }
 
-    public void push(E element) { //add new thing to end of list
-        size++;
-        E[] temp = new E[size];
-        //dont know what to do about the error here
-        for(int i=0; i<size;i++){ //copy array into new array
-            temp[i]=stack[i];
+    public void push(E element) {
+        if (this.currentSize == this.blockSize) {
+            E[] temp = (E[]) new Object[this.blockSize + this.bufferSize];
+            for (int i = 0; i < this.blockSize; i++) {
+                temp[i] = this.stack[i];
+            }
+            this.stack = temp;
+            this.blockSize = this.blockSize + this.bufferSize;
         }
-        temp[size-1]=element;
-        stack=temp;
+        this.stack[this.currentSize] = element;
+        this.currentSize++;
     }
 
-    public E pop() { //return value at top of list
-        size--;
-        E[] temp = new E[size];
-        for(int i=0; i<size;i++){ //copy array into new array
-            temp[i]=stack[i];
+    public E pop() {
+        this.currentSize--;
+        E val = this.stack[this.currentSize];
+        if (this.currentSize <= (this.blockSize - this.bufferSize) && this.blockSize > this.bufferSize) {
+            E[] temp = (E[]) new Object[this.blockSize - this.bufferSize];
+            for (int i = 0; i < (this.blockSize - this.bufferSize); i++) {
+                temp[i] = this.stack[i];
+            }
+            this.stack = temp;
+            this.blockSize = this.blockSize - this.bufferSize;
         }
-        E val = stack[size];
-        stack=temp;
         return val;
     }
 
     public E peek() {
-        return stack[size-1]; //return last value in stack
+        return stack[this.currentSize - 1];
     }
 
     public boolean isEmpty() {
-        return stack.length==0;
+        return this.currentSize == 0;
     }
 
     public int size() {
-        return size;
+        return this.currentSize;
     }
 }
